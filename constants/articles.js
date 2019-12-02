635187535f5ef4a9fc729da8eb26f0cec3d4b9e3
harry-ai <52191038+harry-ai@users.noreply.github.com>
2019-12-02 03:11:18 -1200

    Add files via upload

---
+++ constants/articles.js
@@ -0,0 +1,30 @@
+
+export default [
+  {
+    title: 'Logo design untuk brand anda …',
+    image: 'https://images.unsplash.com/photo-1545231027-637d2f6210f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80',
+    cta: 'View more', 
+    horizontal: true
+  },
+  {
+    title: 'Design kaos premium …',
+    image: 'https://images.unsplash.com/photo-1527718641255-324f8e2d0421?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=701&q=80',
+    cta: 'View more'
+  },
+  {
+    title: 'Buat cartoon dari wajah anda …',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Girl_in_chibi_style.svg/1200px-Girl_in_chibi_style.svg.png',
+    cta: 'View more' 
+  },
+  {
+    title: 'Design poster kekinian …',
+    image: 'https://weplayink.com/wp-content/uploads/2017/10/PosterMCV.jpg',
+    cta: 'View more' 
+  },
+  {
+    title: 'Foto editing …',
+    image: 'https://images.unsplash.com/photo-1503248947681-3198a7abfcc9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80',
+    cta: 'View more', 
+    horizontal: true
+  },
+];
