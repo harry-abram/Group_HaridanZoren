6239f8f3dd5c59804166ee57d64b3b64fd8f6578
ZorenGolioth <zorenfredrik@gmail.com>
2019-12-02 23:13:56 +0800

    Group_HaridanZoren

---
+++ constants/Images.js
@@ -0,0 +1,41 @@
+// local imgs
+const Onboarding = require("../assets/imgs/bg.png");
+const Logo = require("../assets/imgs/argon-logo.png");
+const LogoOnboarding = require("../assets/imgs/argon-logo-onboarding.png");
+const ProfileBackground = require("../assets/imgs/profile-screen-bg.png");
+const RegisterBackground = require("../assets/imgs/register-bg.png");
+const Pro = require("../assets/imgs/getPro-bg.png");
+const ArgonLogo = require("../assets/imgs/argonlogo.png");
+const iOSLogo = require("../assets/imgs/ios.png");
+const androidLogo = require("../assets/imgs/android.png");
+// internet imgs
+
+const ProfilePicture = 'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?fit=crop&w=1650&q=80';
+
+const Viewed = [
+  'https://www.picclickimg.com/d/l400/pict/382401823997_/Fashion-Women-Girl-Korean-Summer-Heart-Print-Casual.jpg',
+  'https://ae01.alicdn.com/kf/HTB1cRR5biDxK1Rjy1zcq6yGeXXa6/HAHAYULE-JBH-Korean-Fashion-Care-Bears-Aesthetic-T-Shirt-Kawaii-Ulzzang-Oversized-Printed-Tee.jpg',
+  'https://d3ui957tjb5bqd.cloudfront.net/uploads/2017/01/Swan-by-Yuri-Kartashev-560x420.jpg',
+  'http://cdn.designbeep.com/wp-content/uploads/2012/10/23.swan-logos.png',
+  'https://img.freepik.com/free-psd/poster-template-summer-festival_23-2148174549.jpg?size=338&ext=jpg',
+  'http://images6.fanpop.com/image/photos/40500000/Jeon-Jungkook-chibi-hatsune_miku257-40589276-736-981.jpg',
+];
+
+const Products = {
+  'Design kami': 'https://cdn.shopify.com/s/files/1/1601/1667/products/product-image-539003640_640x.jpg?v=1549996890',
+};
+
+export default {
+  Onboarding,
+  Logo,
+  LogoOnboarding,
+  ProfileBackground,
+  ProfilePicture,
+  RegisterBackground,
+  Viewed,
+  Products,
+  Pro,
+  ArgonLogo,
+  iOSLogo,
+  androidLogo
+};
